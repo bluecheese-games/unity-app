b6a9141c58fab9affef9de93ca3855a23aebc7c6
Pierre Martin <pierre@bluecheese.games>
2024-06-27 13:21:40 +0200

    Fix MusicOptions

--- Runtime/Services/Audio/MusicOptions.cs
+++ Runtime/Services/Audio/MusicOptions.cs
@@ -2,8 +2,11 @@
 // Copyright (c) 2024 BlueCheese Games All rights reserved
 //
 
+using System;
+
 namespace BlueCheese.App.Services
 {
+    [Serializable]
     public struct MusicOptions
     {
         public float Volume;
