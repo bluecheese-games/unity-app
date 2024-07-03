098058f84ecbf9b22378b520e1b89534e7843a0b
Pierre Martin <pmartin@ohbibi.com>
2024-07-03 14:23:51 +0200

    Services cleanup

--- Tests/Services/Tests_MemoryCacheService.cs
+++ Tests/Services/Tests_MemoryCacheService.cs
@@ -5,126 +5,161 @@
 using NUnit.Framework;
 using BlueCheese.App.Services;
 using System;
+using System.Threading.Tasks;
 
 namespace BlueCheese.Tests.Services
 {
-    public class Tests_MemoryCacheService
-    {
-        private MemoryCacheService _service;
+	public class Tests_MemoryCacheService
+	{
+		private MemoryCacheService _service;
+		private TestClockService _clock;
 
-        [SetUp]
-        public void Setup()
-        {
-            _service = new();
-        }
+		[SetUp]
+		public void Setup()
+		{
+			_clock = new TestClockService();
+			_service = new(_clock);
+		}
 
-        [Test]
-        public void Test_Emptyness()
-        {
-            Assert.That(_service.GetEntries().Count, Is.EqualTo(0));
-        }
+		[Test]
+		public void Test_Emptyness()
+		{
+			Assert.That(_service.GetEntries().Count, Is.EqualTo(0));
+		}
 
-        [Test]
-        public void Test_Not_Exists()
-        {
-            Assert.That(_service.Exists("foo"), Is.False);
-        }
+		[Test]
+		public void Test_Not_Exists()
+		{
+			Assert.That(_service.Exists("foo"), Is.False);
+		}
 
-        [Test]
-        public void Test_Get()
-        {
-            _service.Set("foo", "bar");
+		[Test]
+		public void Test_Get()
+		{
+			_service.Set("foo", "bar");
 
-            Assert.That(_service.Get("foo").Value, Is.EqualTo("bar"));
-        }
+			Assert.That(_service.Get("foo").Value, Is.EqualTo("bar"));
+		}
 
-        [Test]
-        public void Test_GetOrCreate()
-        {
-            Assert.That(_service.GetOrCreate("foo", () => "bar").Value, Is.EqualTo("bar"));
-        }
+		[Test]
+		public void Test_GetOrCreate()
+		{
+			Assert.That(_service.GetOrCreate("foo", () => "bar").Value, Is.EqualTo("bar"));
+		}
 
-        [Test]
-        public void Test_Set()
-        {
-            _service.Set("foo", "bar");
+		[Test]
+		public void Test_Set()
+		{
+			_service.Set("foo", "bar");
 
-            Assert.That(_service.Exists("foo"), Is.True);
-        }
+			Assert.That(_service.Exists("foo"), Is.True);
+		}
 
-        [Test]
-        public void Test_Set_WithExpirationDate()
-        {
-            var expirationDate = DateTime.UtcNow.AddDays(1);
+		[Test]
+		public void Test_Set_WithExpirationDate()
+		{
+			var expirationDate = DateTime.Now.AddDays(1);
 
-            _service.Set("foo", "bar")
-                .WithExpirationDate(expirationDate);
+			_service.Set("foo", "bar")
+				.WithExpirationDate(expirationDate);
 
-            var entry = _service.Get("foo");
+			var entry = _service.Get("foo");
 
-            Assert.That(entry, Is.Not.Null);
-            Assert.That(entry.ExpirationDate, Is.EqualTo(expirationDate));
-            Assert.That(entry.IsExpired, Is.False);
-            Assert.That(entry.HasExpirationDate, Is.True);
-        }
+			Assert.That(entry, Is.Not.Null);
+			Assert.That(entry.ExpirationDate, Is.EqualTo(expirationDate));
+			Assert.That(entry.IsExpired, Is.False);
+			Assert.That(entry.HasExpirationDate, Is.True);
+		}
 
-        [Test]
-        public void Test_Set_WithExpirationDate_Expired()
-        {
-            var expirationDate = DateTime.UtcNow.AddDays(-1);
+		[Test]
+		public void Test_Set_WithExpirationDate_Expired()
+		{
+			var expirationDate = DateTime.Now.AddDays(-1);
 
-            _service.Set("foo", "bar")
-                .WithExpirationDate(expirationDate);
+			_service.Set("foo", "bar")
+				.WithExpirationDate(expirationDate);
 
-            var entry = _service.Get("foo");
+			var entry = _service.Get("foo");
 
-            Assert.That(entry, Is.Null);
-        }
+			Assert.That(entry, Is.Null);
+		}
 
-        [Test]
-        public void Test_Set_WithExpirationTime()
-        {
-            var expirationTime = TimeSpan.FromDays(1);
+		[Test]
+		public void Test_Set_WithExpirationTime()
+		{
+			var expirationTime = TimeSpan.FromDays(1);
 
-            _service.Set("foo", "bar")
-                .WithExpirationTime(expirationTime);
+			_service.Set("foo", "bar")
+				.WithExpirationTime(expirationTime);
 
-            var entry = _service.Get("foo");
+			var entry = _service.Get("foo");
 
-            Assert.That(entry, Is.Not.Null);
-            Assert.That(entry.ExpiresIn, Is.EqualTo(expirationTime).Within(0.001d));
-            Assert.That(entry.IsExpired, Is.False);
-            Assert.That(entry.HasExpirationDate, Is.True);
-        }
+			Assert.That(entry, Is.Not.Null);
+			Assert.That(entry.ExpiresIn, Is.EqualTo(expirationTime).Within(0.001d));
+			Assert.That(entry.IsExpired, Is.False);
+			Assert.That(entry.HasExpirationDate, Is.True);
+		}
 
-        [Test]
-        public void Test_Set_WithExpirationTime_Expired()
-        {
-            var expirationTime = TimeSpan.FromDays(-1);
+		[Test]
+		public void Test_Set_WithExpirationTime_Expired()
+		{
+			var expirationTime = TimeSpan.FromDays(-1);
 
-            _service.Set("foo", "bar")
-                .WithExpirationTime(expirationTime);
+			_service.Set("foo", "bar")
+				.WithExpirationTime(expirationTime);
 
-            var entry = _service.Get("foo");
+			var entry = _service.Get("foo");
 
-            Assert.That(entry, Is.Null);
-        }
+			Assert.That(entry, Is.Null);
+		}
 
-        [Test]
-        public void Test_Change_ExpirationDate()
-        {
-            var expirationDate = DateTime.UtcNow.AddDays(1);
-            var expirationDate2 = DateTime.UtcNow.AddDays(2);
+		[Test]
+		public void Test_Change_ExpirationDate()
+		{
+			var expirationDate = DateTime.Now.AddDays(1);
+			var expirationDate2 = DateTime.Now.AddDays(2);
 
-            _service.Set("foo", "bar")
-                .WithExpirationDate(expirationDate);
+			_service.Set("foo", "bar")
+				.WithExpirationDate(expirationDate);
 
-            Assert.That(_service.Get("foo").ExpiresIn.TotalDays, Is.EqualTo(1).Within(0.001d));
+			Assert.That(_service.Get("foo").ExpiresIn.TotalDays, Is.EqualTo(1).Within(0.001d));
 
-            _service.Set("foo", "bar")
-                .WithExpirationDate(expirationDate2);
+			_service.Set("foo", "bar")
+				.WithExpirationDate(expirationDate2);
 
-            Assert.That(_service.Get("foo").ExpiresIn.TotalDays, Is.EqualTo(2).Within(0.001d));
-        }
-    }
+			Assert.That(_service.Get("foo").ExpiresIn.TotalDays, Is.EqualTo(2).Within(0.001d));
+		}
+
+		private class TestClockService : IClockService
+		{
+			public DateTime Now => _now;
+
+			public event TickEventHandler OnTick;
+			public event Action OnTickSecond;
+
+			private DateTime _now = DateTime.Now;
+
+			public void Initialize() { }
+
+			public Task InvokeAsync(Action action, float delay)
+			{
+				throw new NotImplementedException();
+			}
+
+			public Task WaitAsync(float delay)
+			{
+				throw new NotImplementedException();
+			}
+
+			public void SetNow(DateTime now)
+			{
+				_now = now;
+			}
+
+			public void AddTime(TimeSpan time)
+			{
+				_now += time;
+			}
+		}
+	}
 }
